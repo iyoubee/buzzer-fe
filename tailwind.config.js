600d94b7a15a9b3e2cfa347967210a64cc6fc445
iyoubee <al.ayubi2020@gmail.com>
2023-02-23 06:36:55 +0700

    init setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,12 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: 'Poppins',
+        poppinsBold: 'PoppinsBold',
+      },
+    },
   },
   plugins: [],
 }
